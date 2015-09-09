58de4a5af38482a315a95c238633bed734566ee3
Txuki2005 <ivanlopezmartin1981@gmail.com>
2015-09-09 15:44:59 +0200

    Revert "tick: Call tick_init late"
    This reverts commit fc66210f996d5a8d43901e1f630738eceff9193f.

--- init/main.c
+++ init/main.c
@@ -494,6 +494,7 @@ asmlinkage void __init start_kernel(void)
  * Interrupts are still disabled. Do necessary setups, then
  * enable them
  */
+	tick_init();
 	boot_cpu_init();
 	page_address_init();
 	printk(KERN_NOTICE "%s", linux_banner);
@@ -550,7 +551,6 @@ asmlinkage void __init start_kernel(void)
 	/* init some links before init_ISA_irqs() */
 	early_irq_init();
 	init_IRQ();
-	tick_init();
 	prio_tree_init();
 	init_timers();
 	hrtimers_init();
