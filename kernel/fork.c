9b4400f7d7f502871a287278cc73ef194ad5cd10
Txuki2005 <ivanlopezmartin1981@gmail.com>
2015-09-09 22:05:10 +0200

    Revert "task_allocator: Use config switches instead of magic defines"
    This reverts commit 59846497a3888521e56a6630533f6494cf3362ad.

--- kernel/fork.c
+++ kernel/fork.c
@@ -113,7 +113,7 @@ int nr_processes(void)
 	return total;
 }
 
-#ifndef CONFIG_ARCH_TASK_STRUCT_ALLOCATOR
+#ifndef __HAVE_ARCH_TASK_STRUCT_ALLOCATOR
 static struct kmem_cache *task_struct_cachep;
 
 static inline struct task_struct *alloc_task_struct_node(int node)
@@ -130,7 +130,8 @@ static inline void free_task_struct(struct task_struct *tsk)
 }
 #endif
 
-#ifndef CONFIG_ARCH_THREAD_INFO_ALLOCATOR
+#ifndef __HAVE_ARCH_THREAD_INFO_ALLOCATOR
+
 void __weak arch_release_thread_info(struct thread_info *ti) { }
 
 static struct thread_info *alloc_thread_info_node(struct task_struct *tsk,
@@ -244,7 +245,7 @@ EXPORT_SYMBOL_GPL(__put_task_struct);
 
 void __init fork_init(unsigned long mempages)
 {
-#ifndef CONFIG_ARCH_TASK_STRUCT_ALLOCATOR
+#ifndef __HAVE_ARCH_TASK_STRUCT_ALLOCATOR
 #ifndef ARCH_MIN_TASKALIGN
 #define ARCH_MIN_TASKALIGN	L1_CACHE_BYTES
 #endif
